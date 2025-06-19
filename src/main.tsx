5ae55c91a4a7921650082bb6078719ffe77d1692
Abel Semahegn <abel249733@gmail.com>
2025-06-19 15:56:30 -0500

    react router and stuff will move to other branch

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "@/routes/App";
+import { ThemeProvider } from "@/components/theme-provider"
+import "@/index.css"; 
+const root = document.getElementById("root");
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(root).render(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
+);
---
+++ src/routes/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from '@/assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+  return (
+    <main className='w-full h-full p-10 flex flex-col items-center justify-center'>
+      <div className='w-full h-fit flex flex-col items-center justify-center'>
+        <h1 className='text-7xl text-center font-bold  text-purple-200'>
+          Web Synth
+        </h1>
+      </div>
+
+
+
+
+
+
+    </main>
+  )
+}
