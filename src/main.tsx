013ae915fe00f8233ef63bec9f81d41dfd9c5d62
Abel Semahegn <abel249733@gmail.com>
2025-06-20 19:31:46 -0500

    basic routes and stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import App from "@/routes/App";
+import Home from "@/routes/pages/Home";
+import Start from "@/routes/pages/Start";
+import PreCheck from "@/routes/pages/PreCheck";
+import TrainningRound from "@/routes/pages/TrainningRound";
+import FileUpload from "@/routes/pages/FileUpload";
+
 import { ThemeProvider } from "@/components/theme-provider"
 import "@/index.css"; 
 const root = document.getElementById("root");
@@ -10,7 +14,11 @@ ReactDOM.createRoot(root).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="/precheck" element={<PreCheck />} />
+        <Route path="/trainning" element={<TrainningRound />} />
+        <Route path="/fileupload" element={<FileUpload />} />
       </Routes>
     </BrowserRouter>
   </ThemeProvider>
---
+++ src/routes/pages/Home.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <main className='w-full h-full p-10 flex flex-col items-center justify-center'>
+      <div className="max-w-2xl">
+        <h1 className="md:text-7xl text-lg font-bold text-green-200">Stupid Simple TTS </h1>
+        <h1 className="md:text-7xl text-lg font-bold">for your needs</h1>
+        <p className="font-thin ">
+          If you wanted a simple platform to create a custom tts  you have come to the right place.
+          Web Synth Provides you all you need to create your own custom tts in your browser FOR FREE.
+        </p>
+      </div>
+
+    </main>
+  )
+}
